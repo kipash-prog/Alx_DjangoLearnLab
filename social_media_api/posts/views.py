c2a7f1d2037e1c924a78744a524b235407ca7c97
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-15 20:25:06 +0300

    Update views.py

--- social_media_api/posts/views.py
+++ social_media_api/posts/views.py
@@ -2,8 +2,8 @@ from .models import Post, Comment
 from .serializers import PostSerializers, CommmentSerializers
 from .permissions import IsAuthorOrReadOnly
 from rest_framework import generics, permissions
-from rest_framework import generics, permissions, filters
-
+from rest_framework import generics
+from rest_framework import viewsets, permissions, filters
 class PostCreateView(generics.CreateAPIView):
     queryset = Post.objects.all()
     serializer_class = PostSerializers
@@ -20,7 +20,7 @@ class PostDetailView(generics.RetrieveUpdateDestroyAPIView):
     serializer_class = PostSerializers
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
 
-class CommentListView(generics.ListCreateAPIView):
+class CommentListView(viewsets.ModelViewSet):
     queryset = Comment.objects.all()
     serializer_class = CommmentSerializers
     permission_classes = [permissions.IsAuthenticated]
@@ -35,3 +35,24 @@ class CommentDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommmentSerializers
     permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    queryset = Post.objects.all()
+    serializer_class = PostSerializers
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+    filter_backends = [filters.SearchFilter]
+    search_fields = ['title', 'content']
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user)
+
+class CommentViewSet(viewsets.ModelViewSet):
+    serializer_class = CommmentSerializers
+    permission_classes = [permissions.IsAuthenticated, IsAuthorOrReadOnly]
+
+    def get_queryset(self):
+        return Comment.objects.filter(post_id=self.kwargs['post_id'])
+
+    def perform_create(self, serializer):
+        serializer.save(author=self.request.user, post_id=self.kwargs['post_id'])
