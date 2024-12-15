ddef5206b32d827e352fcc5ea63ff652e39aab98
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-16 02:00:31 +0300

    Update views.py

--- social_media_api/accounts/views.py
+++ social_media_api/accounts/views.py
@@ -28,7 +28,19 @@ class UserDetailView(generics.RetrieveAPIView):
     serializer_class = SocialMediaUserSerializer
     permission_classes = [permissions.IsAuthenticated]
     
+class LoginView(generics.GenericAPIView):
+    serializer_class = LoginSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']
+        token, created = Token.objects.get_or_create(user=user)
+        return Response({
+            'token': token.key,
+            'user_id': user.pk,
+            'username': user.username
+        })
 
 class SocialMediaUserViewSet(viewsets.ModelViewSet):
     queryset = SocialMediaUser.objects.all()
