67ef30631ec70de19c19dd102e97404c57d4aeda
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-16 21:01:50 +0300

    Update models.py

--- social_media_api/posts/models.py
+++ social_media_api/posts/models.py
@@ -10,7 +10,7 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE)
-    followers=models.ManyToManyField('self',symmetrical=False)
+    followings=models.ManyToManyField('self',symmetrical=False)
     def __str__(self):
         return self.title
 
