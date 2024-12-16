470045049ccb3b5c5f0e3033e82329ad824983c1
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-16 21:31:25 +0300

    Update urls.py

--- social_media_api/posts/urls.py
+++ social_media_api/posts/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path,include
-from .views import PostCreateView, PostListView, PostDetailView, CommentListView, CommentDetailView, PostViewSet, CommentViewSet
+from .views import PostCreateView, PostListView, PostDetailView, CommentListView, CommentDetailView, PostViewSet, CommentViewSet,FeedView
 from rest_framework.routers import DefaultRouter
 
 
@@ -12,6 +12,26 @@ router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='po
 
 urlpatterns = [
     path('', include(router.urls)),
+    from django.urls import path,include
+from .views import PostCreateView, PostListView, PostDetailView, CommentListView, CommentDetailView, PostViewSet, CommentViewSet,FeedView
+from rest_framework.routers import DefaultRouter
+
+
+
+
+router = DefaultRouter()
+router.register(r'posts', PostViewSet, basename='post')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='comment')
+router.register(r'posts/(?P<post_id>\d+)/comments', CommentViewSet, basename='post-comments')
+
+urlpatterns = [
+    path('', include(router.urls)),
+    path('feed/',FeedView.as_view(),name="FeedView"),
+    
+     
+    
+]
+ 
     
     
 ]
