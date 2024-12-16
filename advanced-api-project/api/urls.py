d5003f31fbdeb4b8275ff610cc115d84957be284
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-16 17:32:51 +0300

    Update urls.py

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('books/create/', BookCreateView.as_view(), name='book-create'),
     path('books/update/<int:pk>/', BookUpdateView.as_view(), name='book-update'),
     path('books/delete/<int:pk>/', BookDeleteView.as_view(), name='book-delete'),
+    
 ]
