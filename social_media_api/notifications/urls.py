2ecbf91d7489fc38c1c8adf9bd25d59aed8938e9
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-17 00:26:08 +0300

    Delete social_media_api directory

--- social_media_api/notifications/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import NotificationListView, UnreadNotificationListView
-
-urlpatterns = [
-    path('notifications/', NotificationListView.as_view(), name='notifications'),
-    path('notifications/unread/', UnreadNotificationListView.as_view(), name='unread-notifications'),
-]
