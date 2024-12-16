27471985f4572b8d15393a21073f9f4d5f66a7fb
kipash-prog <kidus.UGR-0110-15@aau.edu.et>
2024-12-16 16:39:51 +0300

    Update views.py

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -156,7 +156,7 @@ class CommentDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def get_success_url(self):
         return reverse_lazy('post_detail', kwargs={'pk': self.object.post.pk})
     
-class TagByPostListView(ListView):
+class PostByTagList(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
